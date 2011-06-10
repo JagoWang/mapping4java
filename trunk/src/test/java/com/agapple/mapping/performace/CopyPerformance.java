59efa129c40e6536a285e0f20537a32b5e12dc2c
jianghang115 <jianghang115@d96a5d93-a1f2-f557-1872-b886ed7a2626>
2011-06-10 08:40:47 +0000

    add MapPerformance

--- trunk/src/test/java/com/agapple/mapping/performace/CopyPerformance.java
+++ trunk/src/test/java/com/agapple/mapping/performace/CopyPerformance.java
@@ -1,14 +1,11 @@
 package com.agapple.mapping.performace;
 
-import java.lang.management.ManagementFactory;
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import junit.framework.TestCase;
 import net.sf.cglib.beans.BeanCopier;
 import net.sf.cglib.beans.BulkBean;
 import net.sf.cglib.reflect.FastClass;
@@ -24,15 +21,9 @@ import com.agapple.mapping.BeanCopy;
  * 
  * @author jianghang 2011-5-31 下午02:44:40
  */
-public class CopyPerformance extends TestCase {
-
-    private static final DecimalFormat integerFormat = new DecimalFormat("#,###");
+public class CopyPerformance extends AbstractPerformance {
 
     public static void main(String args[]) throws Exception {
-        testCopy();
-    }
-
-    public static void testCopy() {
         final int testCount = 1000 * 100 * 20;
         CopyBean bean = getBean();
         // BeanMapping copy测试
@@ -232,25 +223,6 @@ public class CopyPerformance extends TestCase {
 
     }
 
-    public static void testTemplate(TestCallback callback, CopyBean source, int count) {
-        int warmup = 10070;
-        // 先进行预热，加载一些类，避免影响测试
-        for (int i = 0; i < warmup; i++) {
-            callback.call(source);
-        }
-        restoreJvm(); // 进行GC回收
-        // 进行测试
-        long start = System.nanoTime();
-        for (int i = 0; i < count; i++) {
-            callback.call(source);
-        }
-        long nscost = (System.nanoTime() - start);
-        System.out.println(callback.getName() + " total cost=" + integerFormat.format(nscost) + "ns , each cost="
-                           + nscost / count + "ns");
-        restoreJvm();// 进行GC回收
-
-    }
-
     private static List<Method> getGetMethod() {
         List<Method> result = new ArrayList<Method>();
         result.add(getMethod("getIntValue", new Class[] {}));
@@ -318,68 +290,4 @@ public class CopyPerformance extends TestCase {
         }
         return result;
     }
-
-    private static CopyBean getBean() {
-        CopyBean bean = new CopyBean();
-        bean.setIntValue(1);
-        bean.setBoolValue(false);
-        bean.setFloatValue(1.0f);
-        bean.setDoubleValue(1.0d);
-        bean.setLongValue(1l);
-        bean.setCharValue('a');
-        bean.setShortValue((short) 1);
-        bean.setByteValue((byte) 1);
-        bean.setIntegerValue(new Integer("1"));
-        bean.setBoolObjValue(new Boolean("false"));
-        bean.setFloatObjValue(new Float("1.0"));
-        bean.setDoubleObjValue(new Double("1.0"));
-        bean.setLongObjValue(new Long("1"));
-        bean.setCharacterValue('a');
-        bean.setShortObjValue(new Short("1"));
-        bean.setByteObjValue(new Byte("1"));
-        bean.setBigIntegerValue(new BigInteger("1"));
-        bean.setBigDecimalValue(new BigDecimal("1"));
-        bean.setStringValue("1");
-        return bean;
-    }
-
-    private static Method getMethod(String methodName, Class... type) {
-        try {
-            return CopyBean.class.getMethod(methodName, type);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private static void restoreJvm() {
-        int maxRestoreJvmLoops = 10;
-        long memUsedPrev = memoryUsed();
-        for (int i = 0; i < maxRestoreJvmLoops; i++) {
-            System.runFinalization();
-            System.gc();
-
-            long memUsedNow = memoryUsed();
-            // break early if have no more finalization and get constant mem used
-            if ((ManagementFactory.getMemoryMXBean().getObjectPendingFinalizationCount() == 0)
-                && (memUsedNow >= memUsedPrev)) {
-                break;
-            } else {
-                memUsedPrev = memUsedNow;
-            }
-        }
-    }
-
-    private static long memoryUsed() {
-        Runtime rt = Runtime.getRuntime();
-        return rt.totalMemory() - rt.freeMemory();
-    }
-
-}
-
-interface TestCallback {
-
-    String getName();
-
-    CopyBean call(CopyBean source);
 }
